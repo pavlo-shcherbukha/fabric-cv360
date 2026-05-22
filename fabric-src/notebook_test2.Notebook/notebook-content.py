8f72238bb868109d476be68da62586e8dec5a882
pavlo-shcherbukha <52211585+pavlo-shcherbukha@users.noreply.github.com>
2026-05-22 07:40:48 +0300

    Committing 2 items from workspace 224e8add-9d35-4fd1-90ec-9f8623492f4c

---
+++ fabric-src/notebook_test2.Notebook/notebook-content.py
@@ -0,0 +1,28 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "environment": {
+# META       "environmentId": "b9eac91d-5b87-a255-4efa-f2fe0c0d3628",
+# META       "workspaceId": "00000000-0000-0000-0000-000000000000"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
